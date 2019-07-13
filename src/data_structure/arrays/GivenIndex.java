125b58d643b1f586b639ad999ce2e14bd65c26db
somratcste <somratcste@gmail.com>
2019-07-14 02:31:54 +0600

    array insert in given index

--- src/data_structure/arrays/GivenIndex.java
+++ src/data_structure/arrays/GivenIndex.java
@@ -1,23 +1,23 @@
 package data_structure.arrays;
 
-public class InsertBeginning {
+public class GivenIndex {
     public static void main(String[] args) {
-        int number = 5;
+        int number = 5, index=3, value=10;
         int[] A = new int[number+1];
+
         for(int i=0; i<number; i++) {
             A[i] = i+1;
         }
 
-        int value = 10;
         System.out.println("Your array: ");
         for(int i=0; i<number; i++) {
             System.out.println(i + " => " + A[i]);
         }
 
-        for(int i= number-1; i>=0; i--) {
+        for(int i= number-1; i>=index-1; i--) {
             A[i+1] = A[i];
         }
-        A[0] = value;
+        A[index-1] = value;
 
         System.out.println("After Inserting: ");
         for(int i=0; i<=number; i++) {
